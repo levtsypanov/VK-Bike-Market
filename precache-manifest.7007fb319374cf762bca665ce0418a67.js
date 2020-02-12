2a32e2797faf420382f41bf54d3df2f84837de35
Лев Цыпанов <51300421+levtsypanov@users.noreply.github.com>
2020-02-12 19:23:39 +0300

    Updates

---
+++ precache-manifest.7007fb319374cf762bca665ce0418a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8ddaf8274e3745f3d77",
+    "url": "/VK-Bike-Market/static/js/runtime~main.2f3d83ed.js"
+  },
+  {
+    "revision": "f49b74ce4cd0cecf7d7e",
+    "url": "/VK-Bike-Market/static/js/main.0f899afc.chunk.js"
+  },
+  {
+    "revision": "58d895f0e47a158c482b",
+    "url": "/VK-Bike-Market/static/js/2.02e6de8c.chunk.js"
+  },
+  {
+    "revision": "f49b74ce4cd0cecf7d7e",
+    "url": "/VK-Bike-Market/static/css/main.5d6e2291.chunk.css"
+  },
+  {
+    "revision": "58d895f0e47a158c482b",
+    "url": "/VK-Bike-Market/static/css/2.e77367fa.chunk.css"
+  },
+  {
+    "revision": "51dd34cb2bb18952fb254e6829d53e7a",
+    "url": "/VK-Bike-Market/index.html"
+  }
+];
